a36b4b3b8df41a93f37ba4f10585dd1138cda011
Amit-Dey <amitdey9020@gmail.com>
2021-03-29 11:08:56 +0600

    Basic calculator

--- Main.java
+++ Main.java
@@ -1,23 +1,63 @@
 package Java.JavaCalculator;
 
-import java.io.PrintStream;
-import java.util.Date;
 import java.util.Scanner;
 
-/**
- * Main
- */
 public class Main {
 
     public static void main(String[] args) {
-        String x= "Hello World";
-        int p=45_4;
-        boolean is =true;
-        Date now =new Date();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Name: ");
-        String s=scanner.next();
-        System.out.println("Hello "+s);
         
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\t\tWelcome to My Calculator\n");
+        System.out.print("Enter an operator and a number:\t");
+
+        long sum=0,a;
+        char c;
+        while(true){
+            System.out.print(sum+" ");
+            c=scanner.next().charAt(0);
+            if (c=='e') {
+                System.out.println("\t\tThank You\n");
+                break;
+            }
+            else if (c=='\n') {
+                sum=0;
+                System.out.println(sum);
+            }
+            else if (c=='h') {
+                System.out.println("\tChar\t\tChar For");
+                System.out.println("\t+\t\tAddition");
+                System.out.println("\t-\t\tSubtraction");
+                System.out.println("\t*\t\tMultiplication");
+                System.out.println("\t/\t\tDivision");
+                System.out.println("\th\t\tHelp");
+                System.out.println("\te\t\tEnd\n");
+
+            }
+            else if (c=='+') {
+                a=scanner.nextLong();
+                sum+=a;
+                System.out.println(sum+"\n");
+            }
+            else if (c=='-') {
+                a=scanner.nextLong();
+                sum-=a;
+                System.out.println(sum+"\n");
+            }
+            else if (c=='*') {
+
+                a=scanner.nextLong();
+                sum*=a;
+                System.out.println(sum+"\n");
+            }
+            else if (c=='/') {
+                a=scanner.nextLong();
+                sum/=a;
+                System.out.println(sum+"\n");
+            }
+            else System.out.println("\tInvalid character..!!");
+
+
+
+        }
     }
 }
